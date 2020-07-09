0251413c0728713020f598f94ac48308b787c7fb
Martin Bodin <martin.bodin@ens-lyon.org>
2020-07-09 20:26:14 +0100

    Adding some measure systems.

--- src/units.ml
+++ src/units.ml
@@ -3,8 +3,8 @@ open Libutils
 
 type kind =
   | Mass
-  | Volume
   | Length
+  | Volume
   | Time
   | Temperature
 
@@ -13,10 +13,13 @@ exception InvalidKind
 (** A unit system describes which base units should be used and when,
    which prefixes should be used, and how to convert them. *)
 type system = {
-    system_id : Id.t (** A unique identifier, to ease comparisons. *) ;
+    system_id : string
+      (** An identifier, to ease comparisons.
+         It must be unique for a given [system_kind]. *) ;
     system_kind : kind (** The kind of the unit system. *) ;
     base_value : float
       (** How the base value relates to the corresponding base value of the metric system. *) ;
+    base_shift : float (** An eventual shift in the zero value. *) ;
     base_notation : string (** The notation of the base unit of this unit system. *) ;
     higher_units : (string * float) list
       (** A list of pairs [(name, value)].
@@ -25,9 +28,6 @@ type system = {
       (** A list of pairs [(value, name)].
          The value represents the number of the next unit that this previous unit is worth of. *)
   }
-
-(** Generate new identifiers for systems. *)
-let new_system_id = Id.new_id_function ()
 
 type t = {
     unit_notation : string (** The notation of the unit. *) ;
@@ -52,12 +52,21 @@ let get_base_unit s = {
     big_unit = false
   }
 
-(** Given a value and a unit, use a higher unit to represent the value. *)
+let kind_name = function
+  | Mass -> "mass"
+  | Volume -> "volume"
+  | Length -> "length"
+  | Time -> "time"
+  | Temperature -> "temperature"
+
+let system_name s =
+  Printf.sprintf "%s-%s" s.system_id (kind_name s.system_kind)
+
 let increase_unit_value (v, u) =
   match u.unit_higher with
-  | [] -> invalid_arg ("increase_unit_value: " ^ print u) (** No bigger unit available. *)
+  | [] -> None (** No bigger unit available. *)
   | (b, q) :: l ->
-    (v /. q, {
+    Some (v /. q, {
       unit_notation = b ;
       unit_system = u.unit_system ;
       unit_higher = l ;
@@ -65,12 +74,11 @@ let increase_unit_value (v, u) =
       big_unit = u.big_unit || u.unit_notation = u.unit_system.base_notation
     })
 
-(** Given a value and a unit, use a lower unit to represent the value. *)
 let decrease_unit_value (v, u) =
   match u.unit_lower with
-  | [] -> invalid_arg ("decrease_unit_value: " ^ print u) (** No bigger unit available. *)
+  | [] -> None (** No bigger unit available. *)
   | (q, b) :: l ->
-    (v *. q, {
+    Some (v *. q, {
       unit_notation = b ;
       unit_system = u.unit_system ;
       unit_higher = (u.unit_notation, q) :: u.unit_higher ;
@@ -78,20 +86,34 @@ let decrease_unit_value (v, u) =
       big_unit = if u.big_unit then b <> u.unit_system.base_notation else false
     })
 
+let system_units s =
+  let u = get_base_unit s in
+  let rec aux acc f u =
+    match f (1., u) with
+    | None -> acc
+    | Some (_, u) -> aux (u :: acc) f u in
+  let ll = List.rev (aux [] decrease_unit_value u) in
+  aux (u :: ll) increase_unit_value u
+
 (** Given a unit, converts it to a value into the base unit of the system. *)
 let rec convert_to_base (v, u) =
   if u.unit_notation = u.unit_system.base_notation then (v, u)
-  else if u.big_unit then convert_to_base (decrease_unit_value (v, u))
-  else convert_to_base (increase_unit_value (v, u))
+  else
+    let f = if u.big_unit then decrease_unit_value else increase_unit_value in
+    let (v, u) = Utils.assert_option __LOC__ (f (v, u)) in
+    convert_to_base (v, u)
 
 (** Normalise a pair value/unit within its own system. *)
 let rec simple_normalise (v, u) =
-  if v < 1. && u.unit_lower <> [] then
-    simple_normalise (decrease_unit_value (v, u))
+  if v < 1. then
+    match decrease_unit_value (v, u) with
+    | Some (v, u) -> simple_normalise (v, u)
+    | None -> (v, u)
   else
     match u.unit_higher with
     | (_, q) :: _ when v > q ->
-      simple_normalise (increase_unit_value (v, u))
+      let (v, u) = Utils.assert_option __LOC__ (increase_unit_value (v, u)) in
+      simple_normalise (v, u)
     | _ -> (v, u)
 
 let normalise s (v, u) =
@@ -100,7 +122,10 @@ let normalise s (v, u) =
     if s.system_id = u.unit_system.system_id then (v, u)
     else
       let (v, u) = convert_to_base (v, u) in
-      (v *. s.base_value /. u.unit_system.base_value, get_base_unit s) in
+      (** First, moving to metric. *)
+      let v = u.unit_system.base_shift +. v *. u.unit_system.base_value in
+      (** Then converting to the new unit *)
+      ((v -. s.base_shift) /. s.base_value, get_base_unit s) in
   simple_normalise (v, u)
 
 (** List of metric prefixes and their associated exponent. *)
@@ -128,50 +153,271 @@ let si_prefixes = [
     ("y", -24)
   ]
 
+(** Create a list of units, given a base unit name and the SI prefixes
+   (with their associated exponent) to use.
+   This function return a pair of lists, one for the higher units, and
+   one for the lower ones, with their associated values. *)
+let create_si_unit base prefixes =
+  let (low, high) = List.partition (fun (_, i) -> i <= 0) prefixes in
+  let low = List.filter (fun (_, i) -> i <> 0) low in
+  let low = List.sort (fun (_, i1) (_, i2) -> - compare i1 i2) low in
+  let low =
+    let rec aux current acc = function
+      | [] -> List.rev acc
+      | (b, exponent) :: low ->
+        let value = Float.pow 10. (Float.of_int (current - exponent)) in
+        aux exponent ((value, b ^ base) :: acc) low in
+    aux 0 [] low in
+  let high = List.sort (fun (_, i1) (_, i2) -> compare i1 i2) high in
+  let high =
+    let rec aux current acc = function
+      | [] -> List.rev acc
+      | (b, exponent) :: high ->
+        let value = Float.pow 10. (Float.of_int (exponent - current)) in
+        aux exponent ((b ^ base, value) :: acc) high in
+    aux 0 [] high in
+  (low, high)
+
+(** A smart constructor for metric-like units. *)
+let create_metric kind name base prefixes =
+  let (lower, higher) = create_si_unit base prefixes in {
+    system_id = name ;
+    system_kind = kind ;
+    base_notation = base ;
+    base_value = 1. ;
+    base_shift = 0. ;
+    higher_units = higher ;
+    lower_units = lower
+  }
+
+(** All the metric systems. *)
+
+(** Mass units. *)
+
+let metric_mass =
+  let s = create_metric Mass "metric" "g" (List.map (fun (p, e) -> (p, e - 3)) si_prefixes) in
+  { s with base_notation = "kg" }
+
+let usual_mass =
+  let lower =
+    let small_prefixes =
+      let prefixes = List.map (fun (p, e) -> (p, e - 3)) si_prefixes in
+      List.filter (fun (_, e) -> e <= 0) prefixes in
+    let (lower, higher) = create_si_unit "g" small_prefixes in
+    assert (higher = []) ;
+    lower in
+  let higher =
+    let large_prefixes =
+      let prefixes = List.map (fun (p, e) -> (p, e + 3)) si_prefixes in
+      List.filter (fun (_, e) -> e >= 3) prefixes in
+    let (lower, higher) = create_si_unit "t" large_prefixes in
+    assert (lower = []) ;
+    higher in {
+    system_id = "usual" ;
+    system_kind = Mass ;
+    base_notation = "kg" ;
+    base_value = 1. ;
+    base_shift = 0. ;
+    higher_units = higher ;
+    lower_units = lower
+  }
+
+(* TODO: common devices (spoons, cups, etc.). *)
+
+(** Length units. *)
+
+let metric_length =
+  create_metric Length "metric" "m" si_prefixes
+
+let usual_length =
+  let prefixes = List.filter (fun (_, e) -> e <= 3) si_prefixes in
+  let s = create_metric Length "usual" "m" prefixes in
+  { s with higher_units =
+            s.higher_units @ [
+                ("R☉", 695_700.) (** Solar radius *) ;
+                ("AU", 215.032155671) (** Astronomical unit *) ;
+                ("ly", 63_241.) (** Light year *) ;
+              ] }
+
+(** Volume units. *)
+
+let metric_volume =
+  create_metric Volume "metric" "m³" (List.map (fun (p, e) -> (p, 3 * e)) si_prefixes)
+
+(* TODO: Usual alternative units (liters, etc.). *)
+
+(** Temperature units. *)
+
+let kelvin =
+  create_metric Temperature "metric" "K" si_prefixes
+
+let celsius = {
+    system_id = "celsius" ;
+    system_kind = Temperature ;
+    base_notation = "°C" ;
+    base_value = 1. ;
+    base_shift = 273.16 ;
+    higher_units = [] ;
+    lower_units = []
+  }
+
+let fahrenheit = {
+    system_id = "fahrenheit" ;
+    system_kind = Temperature ;
+    base_notation = "°F" ;
+    base_value = 0.55555555555555558 ;
+    base_shift = 459.67 ;
+    higher_units = [] ;
+    lower_units = []
+  }
+
+let french_thermostat = {
+    system_id = "fr-thermostat" ;
+    system_kind = Temperature ;
+    base_notation = "Th" ;
+    base_value = 27.77777777777778 ;
+    base_shift = 300.937777778 ;
+    higher_units = [] ;
+    lower_units = []
+  }
+
+let german_thermostat = {
+    system_id = "de-Stufe" ;
+    system_kind = Temperature ;
+    base_notation = "Stufe" ;
+    base_value = 25. ;
+    base_shift = 398.16 ;
+    higher_units = [] ;
+    lower_units = []
+  }
+
+let gas_mark = {
+    system_id = "uk-gas-mark" ;
+    system_kind = Temperature ;
+    base_notation = "Gas Mark" ;
+    base_value = 13.88888888888889 ;
+    base_shift = 383.16 ;
+    higher_units = [] ;
+    lower_units = []
+  }
+
 let all_systems =
-  ignore (new_system_id, si_prefixes) ;
-  PMap.empty
-  (* TODO: Metric with common abbreviations (e.g. megatons instead of gigagrams,
-     and only using multiples of 3 for prefixes),
-     metric, common devices (spoons, cups, etc.), etc. *)
+  let m = PMap.empty in
+  let m =
+    PMap.add Mass [
+        usual_mass ;
+        metric_mass
+      ] m in
+  let m =
+    PMap.add Length [
+        metric_length ;
+        usual_length
+      ] m in
+  let m =
+    PMap.add Volume [
+        metric_volume
+      ] m in
+  let m = PMap.add Time [ (* TODO *) ] m in
+  let m =
+    PMap.add Temperature [
+        celsius ;
+        fahrenheit ;
+        kelvin ;
+        french_thermostat ;
+        german_thermostat ;
+        gas_mark
+      ] m in
+  m
+
+let%test _ =
+  (** System names are unique. *)
+  PMap.fold (fun l b ->
+    b &&
+      let l = List.mapi (fun i s -> (i, system_name s)) l in
+      List.for_all (fun (i1, u1) ->
+        List.for_all (fun (i2, u2) ->
+          let b = i1 = i2 || u1 <> u2 in
+          if not b then Printf.printf "System defined twice: %s (indices %d and %d).\n%!" u1 i1 i2 ;
+          b) l) l) all_systems true
+
+let%test_unit _ =
+  (** State that two values are close enough one to the other. *)
+  let close_enough u v1 v2 =
+    let b = 0.999 *. v1 <= v2 && v2 <= 1.001 *. v1 in
+    if not b then Printf.printf "Unit %s defined twice, but %F ≠ %F.\n%!" u v1 v2 ;
+    b in
+  (** If two units have the same name, then they convert one to another. *)
+  ignore (PMap.foldi (fun kind l units ->
+    List.fold_left (fun units s ->
+      List.fold_left (fun units u ->
+          assert (kind = get_kind u) ;
+          if PMap.mem u.unit_notation units then (
+            (* There is a name conflict: let us check that these units convert
+               well one to the other.  *)
+            let (u', s') = PMap.find u.unit_notation units in
+            assert (get_kind u = get_kind u')
+              (* Their kinds are the same. *) ;
+            assert (
+              let b = system_name s <> system_name s' in
+              if not b then
+                Printf.printf "System name clash: %s in %s.\n%!" (print u) (system_name s) ;
+              b) (* Two units in the same system never clash. *) ;
+            let (v1, u1) = normalise s (1., u') in
+            assert (
+              let b = print u1 = print u in
+              if not b then
+                Printf.printf "Systems normalise differently: %s vs %s.\n%!" (print u1) (print u) ;
+              b) ;
+            assert (close_enough (print u) 1. v1) ;
+            let (v2, u2) = normalise s' (1., u) in
+            assert (
+              let b = print u2 = print u' in
+              if not b then
+                Printf.printf "Systems normalise differently: %s vs %s.\n%!" (print u1) (print u) ;
+              b) ;
+            assert (close_enough (print u) 1. v2) ;
+            units
+          ) else PMap.add u.unit_notation (u, s) units)
+        units (system_units s)) units l) all_systems PMap.empty)
 
 let parse str =
   let str = String.trim str in
-  PMap.fold (fun s -> function
+  PMap.fold (fun l -> function
     | Some r -> Some r
     | None ->
-      if str = s.base_notation then Some (get_base_unit s)
-      else
-        let rec aux current higher = function
-          | [] -> None
-          | (q, b) :: lower ->
-            let higher = (current, q) :: higher in
-            if str = b then
-              Some {
-                  unit_notation = b ;
-                  unit_system = s ;
-                  unit_higher = higher ;
-                  unit_lower = lower ;
-                  big_unit = false
-                }
-            else aux b higher lower in
-        match aux s.base_notation s.higher_units s.lower_units with
-        | Some r -> Some r
-        | None ->
-          let rec aux current lower = function
-            | [] -> None
-            | (b, q) :: higher ->
-              let lower = (q, current) :: lower in
-              if str = b then
-                Some {
-                  unit_notation = b ;
-                  unit_system = s ;
-                  unit_higher = higher ;
-                  unit_lower = lower ;
-                  big_unit = true
-                  }
-              else aux b lower higher in
-          aux s.base_notation s.lower_units s.higher_units) all_systems None
-
-(* TODO: Unit tests. *)
+      List.fold_left (function
+        | Some r -> fun _ -> Some r
+        | None -> fun s ->
+          if str = s.base_notation then Some (get_base_unit s)
+          else
+            let rec aux current higher = function
+              | [] -> None
+              | (q, b) :: lower ->
+                let higher = (current, q) :: higher in
+                if str = b then
+                  Some {
+                      unit_notation = b ;
+                      unit_system = s ;
+                      unit_higher = higher ;
+                      unit_lower = lower ;
+                      big_unit = false
+                    }
+                else aux b higher lower in
+            match aux s.base_notation s.higher_units s.lower_units with
+            | Some r -> Some r
+            | None ->
+              let rec aux current lower = function
+                | [] -> None
+                | (b, q) :: higher ->
+                  let lower = (q, current) :: lower in
+                  if str = b then
+                    Some {
+                      unit_notation = b ;
+                      unit_system = s ;
+                      unit_higher = higher ;
+                      unit_lower = lower ;
+                      big_unit = true
+                      }
+                  else aux b lower higher in
+              aux s.base_notation s.lower_units s.higher_units) None l) all_systems None
 
--- src/units.mli
+++ src/units.mli
@@ -13,8 +13,8 @@ val print : t -> string
 (** The kind of the unit. *)
 type kind =
   | Mass
-  | Volume
   | Length
+  | Volume
   | Time
   | Temperature
 
@@ -24,21 +24,35 @@ val get_kind : t -> kind
 (** A unit system (e.g. metric) *)
 type system
 
+(** Get a name for a system. *)
+val system_name : system -> string
+
 (** Each system is only made for a specific kind of units. *)
 val get_system_kind : system -> kind
 
 (** Given a system, returns its base unit. *)
 val get_base_unit : system -> t
 
+(** All the units defined in a system. *)
+val system_units : system -> t list
+
 (** All the available systems, for each unit kinds.
    The first one is usually the most preferable one. *)
-val all_systems : (kind, system) PMap.t
+val all_systems : (kind, system list) PMap.t
 
 exception InvalidKind
 
 (** Given a unit system, normalise a value.
    Raises [InvalidKind] if the system kind does not corresponds to the given unit kind. *)
 val normalise : system -> float * t -> float * t
+
+(** Given a value and a unit, use a higher unit to represent the value.
+   Return [None] if there is no higher avalaible units in the current unit system. *)
+val increase_unit_value : float * t -> (float * t) option
+
+(** Given a value and a unit, use a lower unit to represent the value.
+   Return [None] if there is no lower avalaible units in the current unit system. *)
+val decrease_unit_value : float * t -> (float * t) option
 
 (* TODO: Import/Export functions *)
 
